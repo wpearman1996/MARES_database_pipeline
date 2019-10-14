167778c947ce92ee677beaa264f5651fc87e5f48
vanearranz <55219129+vanearranz@users.noreply.github.com>
2019-10-14 16:27:27 +1300

    Update step4b_taxid_processing.sh

--- step4b_taxid_processing.sh
+++ step4b_taxid_processing.sh
@@ -3,12 +3,13 @@ cd ./taxid_process
 #After executing the R script - we then process the sequences
 sed 's/^.\{1\}//' seqs_oldnames.txt > seqs_oldnames2.txt
 sed 's/^.\{1\}//' seqs_newnames.txt > seqs_newnames2.txt
-sed -i -e 's/^/>/' seqs_newnames2.txt
+#sed -i -e 's/^/>/' seqs_newnames2.txt
 #sed -i -e 's/^/>/' seqs_oldnames2.txt
 seqtk subseq Marine_Euk_BOLD_NCBI_final_sl.fasta seqs_oldnames2.txt > Marine_Euk_BOLD_NCBI_sl_s4.fasta
 grep ">" Marine_Euk_BOLD_NCBI_sl_s4.fasta > sl_s4_seqnames.txt
 sed -i -e 's/>//' sl_s4_seqnames.txt
-grep -F -f sl_s4_seqnames.txt seqs_newnames2.txt > seqs_newnames3.txt
+Rscript ../newname_filt.r
 awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < ./Marine_Euk_BOLD_NCBI_sl_s4.fasta > ./Marine_Euk_BOLD_NCBI_final_sl_s5.fasta
 tail -n +2 Marine_Euk_BOLD_NCBI_final_sl_s5.fasta > Marine_Euk_BOLD_NCBI_final_sl_s6.fasta
-awk 'NR%2==0' Marine_Euk_BOLD_NCBI_final_sl_s6.fasta | paste -d'\n' seqs_newnames3.txt - > Marine_Euk_BOLD_NCBI_sl_reformatted.fasta
+#awk 'BEGIN {RS = ">" ; FS = "\n" ; ORS = ""} {if ($2) print ">"$0}' Marine_Euk_BOLD_NCBI_final_sl_s6.fasta > Marine_Euk_BOLD_NCBI_final_sl_s7.fasta
+awk 'NR%2==0' Marine_Euk_BOLD_NCBI_final_sl_s6.fasta | paste -d'\n' newnames3.txt - > Marine_Euk_BOLD_NCBI_sl_reformatted.fasta
