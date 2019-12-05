09c4f7d31db7fc2f084d2af01d7fefc1854db0a5
vanearranz <55219129+vanearranz@users.noreply.github.com>
2019-12-05 15:02:36 +1300

    Update newname_filt.r

--- newname_filt.r
+++ newname_filt.r
@@ -1,6 +1,8 @@
 x<-readLines("sl_s4_seqnames.txt")
 y<-readLines("seqs_newnames2.txt")
 t<-data.frame(y,stringr::word(y,1))
-t1<-t[t$stringr..word.y..1. %in% x,]
+t$stringr..word.y..1.<-as.character(t$stringr..word.y..1.)
+x2<-stringr::word(x,1)
+t1<-t[t$stringr..word.y..1. %in% x2,]
 t1$y<-paste0(">",t1$y)
 writeLines(as.character(t1$y),"newnames3.txt",)
