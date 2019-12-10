a2353ff1195fd0a9ade0a99766f2da484cbcbafe
wpearman1996 <38397623+wpearman1996@users.noreply.github.com>
2019-12-11 10:20:07 +1300

    Update step5b_prepare_to_MEGAN.sh

--- step5b_prepare_to_MEGAN.sh
+++ step5b_prepare_to_MEGAN.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-#taxon= "Marine_Euk"
-#query= "query"
-
-#1. Build a database
-
 mkdir MEGAN_db
 makeblastdb -in taxid_process/Marine_Euk_BOLD_NCBI_sl_reformatted.fasta -dbtype nucl -parse_seqids
