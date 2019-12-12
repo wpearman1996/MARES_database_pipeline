367d2e96c728b74d33b1b67d7a3faea87470f2b1
vanearranz <55219129+vanearranz@users.noreply.github.com>
2019-12-12 13:25:24 +1300

    Update step5b_prepare_to_MEGAN.sh

--- step5b_prepare_to_MEGAN.sh
+++ step5b_prepare_to_MEGAN.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-makeblastdb -in taxid_process/Marine_Euk_BOLD_NCBI_sl_reformatted.fasta -dbtype nucl -parse_seqids -out MARES_BAR.db
+makeblastdb -in taxid_process/MARES_BAR_BOLD_NCBI_sl_reformatted.fasta -dbtype nucl -parse_seqids -out MARES_BAR.db
 mkdir ../MEGAN_db
 mv MARES_BAR.db* ../BAR_MEGAN_db
