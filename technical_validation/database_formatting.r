1f228c5a3bc02c698c01108d6d0eea7c17861a0e
wpearman1996 <38397623+wpearman1996@users.noreply.github.com>
2019-12-17 10:09:22 +1300

    Update database_formatting.r

--- technical_validation/database_formatting.r
+++ technical_validation/database_formatting.r
@@ -1,5 +1,4 @@
-# Database comparisons
-
+# Database Comparisons
 library(stringr)
 library(taxize)
 library(stringi)
@@ -20,8 +19,7 @@ mbpkdb2<-gsub("'","",mbpkdb2)
 mbpkdb2<-mbpkdb2[!grepl("\\.",mbpkdb2)]
 mbpk_acc<-mbpkdb2[grepl(">",mbpkdb2)]
 mbpk_acc <- gsub(">","",mbpk_acc)
-ENTREZ_KEY<-"QWERTY" # INSERT YOUR ENTREZ KEY HERE
-acc_det<-genbank2uid(mbpk_acc,key = ENTREZ_KEY)
+acc_det<-genbank2uid(mbpk_acc,key = "QWERTY") ## Insert your entrez key here
 acc_det_tax<-do.call("rbind",acc_det)
 acc_det_tax<-acc_det_tax[!is.na(acc_det_tax)]
 taxnames_mbpkacc<-id2name(acc_det_tax,db="ncbi")
@@ -41,7 +39,7 @@ mbpkdb2<-(mbpkdb2[t !=0])
 
 ## 2. Genbank Eukaryota COI without the Keyword = Barcode (Benson et al. 2015)
 
-genbank<-read.delim("./Reference_db/genbank_seqnames.txt",head=F,sep="|")
+genbank<-read.delim("./Reference_db//genbank_seqnames.txt",head=F,sep="|")
 genbank<-unique(genbank$V2)
 genbank<-as.character(genbank)
 genbank<-(genbank[!grepl("\\d",genbank)])
@@ -61,7 +59,7 @@ genbank<-unique(genbank)
 
 ## 3. BOLD all taxon list (Ratnasingham and Hebert 2007)
 
-bold<-read.delim("./Reference_db/bold_seqnames.txt",head=F,sep="|")
+bold<-read.delim("./Reference_db//bold_seqnames.txt",head=F,sep="|")
 bold<-bold$V2
 bold<-as.character(unique(bold))
 bold<-(bold[!grepl("\\d",bold)])
@@ -76,7 +74,7 @@ bold<-unique(bold)
 
 ## 4. Anacapa COI BOLD all taxon list (Curd et al. 2019)
 
-anacapa<-read.delim("Reference_db/anacapa_seqnames.txt",head=F)
+anacapa<-read.delim("./Reference_db//anacapa_CO1_taxonomy.txt",head=F)
 anacapa_spec<-sub('.*\\;', '', anacapa$V2)
 anacapa_spec<-stringr::word(anacapa_spec,1,2)
 
@@ -89,7 +87,7 @@ anacapa_spec<-unique(anacapa_spec)
 
 ## 5. MIDORI COI_LONGEST (Machida et al. 2017)
 
-midori<-read.delim("Reference_db/MIDORI_LONGEST_20180221_COI.taxon",
+midori<-read.delim("./Reference_db//MIDORI_LONGEST_20180221_COI.taxon",
                    head=F,sep=";")
 midori_spec<-(midori$V7)
 midori<-(midori[!grepl("\\d",midori)])
@@ -108,7 +106,7 @@ midori_spec[8]<-NA;midori_spec<-midori_spec[!is.na(midori_spec)]
 
 ## 6. MARES_wo_barcode
 
-mares_wo<-read.delim("Reference_db/MARES_wo_barcodes_seqnames.txt",sep=" ",stringsAsFactors=FALSE,head=F)
+mares_wo<-read.delim("./Reference_db//MARES_wo_barcodes_seqnames.txt",sep=" ",stringsAsFactors=FALSE,head=F)
 mares_wo<-paste(mares_wo$V2,mares_wo$V3)
 mares_wo<-unique(mares_wo)
 mares_wo<-(mares_wo[!grepl("\\d",mares_wo)])
@@ -127,7 +125,7 @@ mares_wo<-mares_wo[!ifelse(word(mares_wo,2)=="sp",TRUE,FALSE)]
 
 ## 6. MARES_w_barcode
 
-mares_bar<-read.delim("Reference_db/MARES_barcode_seqnames.txt",sep=" ",stringsAsFactors=FALSE,head=F)
+mares_bar<-read.delim("./Reference_db/MARES_barcode_seqnames.txt",sep=" ",stringsAsFactors=FALSE,head=F)
 mares_bar<-paste(mares_bar$V2,mares_bar$V3)
 mares_bar<-unique(mares_bar)
 mares_bar<-(mares_bar[!grepl("\\d",mares_bar)])
@@ -167,22 +165,17 @@ midori_spec<-midori_spec[midori_spec %in% all_spec]
 bold<-bold[bold %in% all_spec]
 genbank<-genbank[genbank %in% all_spec]
 mares_bar<-mares_bar[mares_bar %in% all_spec]
-mares_wo<-mares_wo[me_wo %in% all_spec]
+mares_wo<-mares_wo[mares_wo %in% all_spec]
 mbpkdb2<-mbpkdb2[mbpkdb2 %in% all_spec]
 anacapa_spec<-anacapa_spec[anacapa_spec %in% all_spec]
 
+worms_spec<-read.delim("./Reference_db/taxon.txt",stringsAsFactors = F) ## Note to do this, you must have a local copy of the worms database, which requires authorized access. As such we have not included this file in our dataset.
 
-out <- t(splitstackshape:::charMat(listOfValues = mget(c("midori_spec","bold","genbank",
-                "mbpkdb2","mares_wo","mares_bar","anacapa_spec")), fill = 0L))
-fac<-c("midori","bold","genbank",
-                "mbpkdb","mares_wo","mares_bar","anacapa")
-colnames(out)<-fac
-
-outsub<-out[sample(nrow(out), 1000), ]
-write.csv(out,"./presab_allspec_alldatabases.csv")
-
-
-
-
-
+length(mares_bar[mares_bar %in% worms_spec$scientificName])/length(mares_bar)
+length(mares_wo[mares_wo %in% worms_spec$scientificName])/length(mares_wo)
+length(genbank[genbank %in% worms_spec$scientificName])/length(genbank)
+length(midori_spec[midori_spec %in% worms_spec$scientificName])/length(midori_spec)
+length(bold[bold %in% worms_spec$scientificName])/length(bold)
+length(mbpkdb2[mbpkdb2 %in% worms_spec$scientificName])/length(mbpkdb2)
+length(anacapa_spec[anacapa_spec %in% worms_spec$scientificName])/length(anacapa_spec)
 
