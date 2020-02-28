e779c54acd6165d2de67905ae83891bd62e1d6f3
wpearman1996 <38397623+wpearman1996@users.noreply.github.com>
2020-02-28 18:40:19 +1300

    Update step4_taxid_generation.sh

--- step4_taxid_generation.sh
+++ step4_taxid_generation.sh
@@ -1,3 +1,4 @@
 cat ./taxid_process/notaxid_forgeneration.csv | \
 awk -F "," '{ print "perl ./taxdump_edit.pl -names names.dmp -nodes nodes.dmp -taxa "$1" -parent "$3" -rank genus -division 1" }' | \
 while read cmd; do $cmd >> details_newtaxids.txt; done
+grep "TaxID" details_newtaxids.txt > newtaxids.txt
