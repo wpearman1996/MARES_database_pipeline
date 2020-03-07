f8f5e2fe9065e328dfb4419a429567750ab440a2
wpearman1996 <38397623+wpearman1996@users.noreply.github.com>
2020-03-08 09:11:56 +1300

    Update step4b_taxid_generation.sh

--- step4b_taxid_generation.sh
+++ step4b_taxid_generation.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 echo '#!/bin/bash' | cat - ./taxid_commands_addition.txt > perl_taxid_additioncommands.sh
 chmod +x perl_taxid_additioncommands.sh
 ./perl_taxid_additioncommands.sh > details_newtaxids.txt
