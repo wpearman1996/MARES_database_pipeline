f62bf1f64f6a087a0e3b251a5501a9b0ed9804f7
wpearman1996 <38397623+wpearman1996@users.noreply.github.com>
2020-03-28 14:03:08 +1300

    Update step6_marine_contaminants_checker.R

--- step6_marine_contaminants_checker.R
+++ step6_marine_contaminants_checker.R
@@ -3,11 +3,11 @@
 # AlgaeBase is not included in the standard worms download, thus this should be used as a tool to filter by, rather
 # should be used as a tool to aid in data exploration. 
 worms <- read.delim("./worms_taxonlist.txt") 
-contam <- readLines("./custom_metabarcoding_databases_contam/taxa.list") 
+contam <- readLines("../custom_metabarcoding_databases_contam//taxa.list") 
 
-kraken <- read.delim
 
-megan <- read.csv("./rep-seqs-nofilt-assigned.csv")
+
+megan <- read.csv("../rep-seqs-nofilt-assigned.csv")
 library(stringr); library(plyr)
 megan_taxonpath <- str_split(megan$TaxonPath,pattern=";")
 megan$Genus <- (str_match(megan$TaxonPath, "g__(.*?);"))[,2]
@@ -18,3 +18,25 @@ megan$Species_Marine <- ifelse(megan$Species %in% worms$acceptedNameUsage, TRUE,
 megan$Genus_Contam <- ifelse(megan$Genus %in% word(contam,1), TRUE,FALSE)
 megan$Species_Contam <- ifelse(megan$Species %in% contam, TRUE,FALSE)
 write.csv(megan,"megan_contam_marine_list.csv")
+
+
+#The following code to import the names.dmp file was retrieved from taxizedb
+ncbi_names_file <- #location of names.dmp file.
+ncbi_names <- readr::read_tsv(
+  ncbi_names_file,
+  col_names = c("tax_id", "name_txt", "unique_name", "name_class"),
+  col_type = "i_c_c_c_",
+  quote = ""
+)
+
+ncbi_names<-ncbi_names[ncbi_names$name_class=="scientific name",]
+
+kraken <- read.delim("../kraken_test.txt",head=F)
+colnames(kraken)<- c("ClassifiedStatus","ReadID","TaxID","SequenceLength","LCA_History")
+kraken$TaxonClass <- ncbi_names$name_txt[match(kraken$V3,ncbi_names$tax_id)]
+kraken$Genus <- word(kraken$TaxonClass,1)
+kraken$Genus_Marine <- ifelse(kraken$Genus %in% worms$genus, TRUE,FALSE)
+kraken$Species_Marine <- ifelse(kraken$TaxonClass %in% worms$acceptedNameUsage, TRUE,FALSE)
+kraken$Genus_Contam <- ifelse(kraken$Genus %in% word(contam,1), TRUE,FALSE)
+kraken$Species_Contam <- ifelse(kraken$Species %in% contam, TRUE,FALSE)
+write.csv(kraken,"kraken_contam_marine_list.csv")
