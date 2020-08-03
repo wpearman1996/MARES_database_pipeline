c095dfb2ad54561baee1368066e00bebf273e75a
wpearman1996 <38397623+wpearman1996@users.noreply.github.com>
2020-08-03 19:45:13 +1200

    Update step5b_prepare_to_MEGAN.sh

--- step5b_prepare_to_MEGAN.sh
+++ step5b_prepare_to_MEGAN.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cut -f1 -d" " MARES_BAR_BOLD_NCBI_sl_reformatted.fasta > MARES_BAR_BOLD_NCBI_sl_reformatted_blast.fasta
-awk 'NR % 2 == 1' MARES_NOBAR_BOLD_NCBI_sl_reformatted.fasta | sed 's|[>,]||g' - | sed -e 's/ /\t/g' - > MARES_informative_name_table.tsv
+awk 'NR % 2 == 1' MARES_NOBAR_BOLD_NCBI_sl_reformatted.fasta | sed 's|[>,]||g' - > MARES_informative_name_table.tsv
 awk 'NR % 2 == 1' MARES_BAR_BOLD_NCBI_sl_reformatted.fasta  | awk -F" " '{print $(NF-1)}' > mares_taxids.txt
 awk 'NR % 2 == 1' MARES_BAR_BOLD_NCBI_sl_reformatted_blast.fasta | sed 's|[>,]||g' - > seqnames_mares.txt
 paste seqnames_mares.txt mares_taxids.txt | column -s $' ' -t > cust_taxid_map
