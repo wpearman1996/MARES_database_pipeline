bba48ec8ff07dc11db4ebfcf9c4b30b33547f432
wpearman1996 <38397623+wpearman1996@users.noreply.github.com>
2021-01-25 09:44:40 +1300

    Update NPerc.R

--- NPerc.R
+++ NPerc.R
@@ -1,5 +1,15 @@
 library(readr)
 library(stringr)
+library("optparse")
+option_list = list(
+  make_option(c("-p","--percent"), type="numeric", default=NULL, 
+              help="Percent N to remove", metavar="numeric"))
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+if (is.null(opt$percent)){
+  print_help(opt_parser)
+  stop("Percent N is required", call.=FALSE)
+}
 x<-read.delim("./basecounts.txt",sep="\t",head=F,na.strings=c(" ",""))
 
 
@@ -29,5 +39,5 @@ x$N<-ifelse(grepl("N",x$V2), parse_number(x$V2),
                           ifelse(grepl("N",x$V5), parse_number(x$V5),parse_number(x$V6)))))
 x$NPerc<-100*(x$N/(x$A+x$T + x$G + x$C + x$N))
 x$NPerc<-ifelse(is.na(x$NPerc),0,x$NPerc)
-
+x<-x[x$NPerc <= opt$percent,]
 write.table(x[,c(1,12)],"./NPercs.txt")
