fb2ce4b5e186cfec672355a26eb5c59742e2bf32
vanearranz <55219129+vanearranz@users.noreply.github.com>
2021-10-11 18:59:47 +1300

    Update step4e more generic

--- step4e_Ncorrection.sh
+++ step4e_Ncorrection.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-cd ./taxid_process
-#sed '1d' MARES_BAR_BOLD_NCBI_sl_reformatted.fas.fasta > tmpfile; mv tmpfile Marine_Euk_BOLD_NCBI_final_sl.fasta 
-perl ../perl_NCounter.pl MARES_BAR_BOLD_NCBI_sl_reformatted.fasta > basecounts.txt
-grep ">" MARES_BAR_BOLD_NCBI_sl_reformatted.fasta > seqnames_mares_reform.txt
-Rscript ../NPerc.R -p 10 #change to percent N you want to remove greater than
-seqtk subseq MARES_BAR_BOLD_NCBI_sl_reformatted.fasta NPercs.txt > tmpfile.txt
-mv tmpfile.txt MARES_BAR_BOLD_NCBI_sl_reformatted.fasta
 
+database="Marine_Euk"
+
+cd ./taxid_process
+#sed '1d' ${database}_BOLD_NCBI_sl_reformatted.fas.fasta > tmpfile; mv tmpfile ${database}_BOLD_NCBI_final_sl.fasta
+perl ../perl_NCounter.pl ${database}_BOLD_NCBI_sl_reformatted.fasta > basecounts.txt
+grep ">" ${database}_BOLD_NCBI_sl_reformatted.fasta > ${database}_reform.txt
+Rscript ../NPerc.R -p 10 #change to percent N you want to remove greater than
+seqtk subseq ${database}_BOLD_NCBI_sl_reformatted.fasta NPercs.txt > tmpfile.txt
+mv tmpfile.txt ${database}_BOLD_NCBI_sl_reformatted.fasta
