ddd856a2f47de978d45d04f55bf3c9c663fe279d
vanearranz <55219129+vanearranz@users.noreply.github.com>
2021-10-12 12:29:51 +1300

    Update step5_make_krakendb.sh

--- step5_make_krakendb.sh
+++ step5_make_krakendb.sh
@@ -9,7 +9,7 @@ sed -i 's/ //g' kraken_taxids.txt
 sed -i -e 's/^/>kraken:taxid|/' kraken_taxids.txt
 awk 'NR%2==0' ${database}_BOLD_NCBI_sl_reformatted.fasta | paste -d'\n' kraken_taxids.txt - > ${database}_BOLD_NCBI_sl_kraken.fasta
 cd ..
-kraken2-build --download-taxonomy --db mares 
+kraken2-build --download-taxonomy --db mares_kraken_db 
 cp ./cust_accession2taxid ./mares/taxonomy/cust.accession2taxid
 kraken2-build --add-to-library ./taxid_process/${database}_BOLD_NCBI_sl_kraken.fasta --db mares
 kraken2-build --build --db mares
