53941a0bb510ebd355353db786cb7de773dfabc5
vanearranz <55219129+vanearranz@users.noreply.github.com>
2021-10-12 16:48:00 +1300

    Update for Docker and more general

--- step5b_prepare_to_MEGAN.sh
+++ step5b_prepare_to_MEGAN.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
+database="database"
+
 cd taxid_process
-cut -f1 -d" " MARES_BAR_BOLD_NCBI_sl_reformatted.fasta > MARES_BAR_BOLD_NCBI_sl_reformatted_blast.fasta
-awk 'NR % 2 == 1' MARES_BAR_BOLD_NCBI_sl_reformatted.fasta | sed 's|[>,]||g' - > MARES_informative_name_table.tsv
-awk 'NR % 2 == 1' MARES_BAR_BOLD_NCBI_sl_reformatted.fasta  | awk -F" " '{print $(NF-1)}' > mares_taxids.txt
-awk 'NR % 2 == 1' MARES_BAR_BOLD_NCBI_sl_reformatted_blast.fasta | sed 's|[>,]||g' - > seqnames_mares.txt
-#paste seqnames_mares.txt mares_taxids.txt | column -s $' ' -t > cust_taxid_map
-# With Docker didn't find column command  
-paste seqnames_mares.txt mares_taxids.txt > cust_taxid_map
-makeblastdb -in MARES_BAR_BOLD_NCBI_sl_reformatted_blast.fasta -dbtype nucl -taxid_map cust_taxid_map -parse_seqids -out MARES_BAR.db
+cut -f1 -d" " ${database}_BOLD_NCBI_sl_reformatted.fasta > ${database}_BAR_BOLD_NCBI_sl_reformatted_blast.fasta
+awk 'NR % 2 == 1' ${database}_BOLD_NCBI_sl_reformatted.fasta | sed 's|[>,]||g' - > ${database}_informative_name_table.tsv
+awk 'NR % 2 == 1' ${database}_BOLD_NCBI_sl_reformatted.fasta  | awk -F" " '{print $(NF-1)}' > m${database}_taxids.txt
+awk 'NR % 2 == 1' ${database}_BOLD_NCBI_sl_reformatted_blast.fasta | sed 's|[>,]||g' - > ${database}_seqnames.txt
+paste ${database}_seqnames.txt ${database}_taxids.txt > cust_taxid_map
+
+makeblastdb -in ${database}_BOLD_NCBI_sl_reformatted_blast.fasta -dbtype nucl -taxid_map cust_taxid_map -parse_seqids -out ${database}.db
 mkdir ../MEGAN_db
-mv MARES_BAR.db* ../BAR_MEGAN_db
+mv ${database}.db* ../MEGAN_db
