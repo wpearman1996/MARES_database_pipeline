0afc08a988b95f9bb0a15a6906bd1a7c97db5513
vanearranz <55219129+vanearranz@users.noreply.github.com>
2021-10-12 18:18:53 +1300

    Update step4e_Ncorrection.sh

--- step4e_Ncorrection.sh
+++ step4e_Ncorrection.sh
@@ -9,3 +9,6 @@ grep ">" ${database}_BOLD_NCBI_sl_reformatted.fasta > ${database}_reform.txt
 Rscript ../NPerc.R -p 10 #change to percent N you want to remove greater than
 seqtk subseq ${database}_BOLD_NCBI_sl_reformatted.fasta NPercs.txt > tmpfile.txt
 mv tmpfile.txt ${database}_BOLD_NCBI_sl_reformatted.fasta
+
+mkdir ../${database}
+cp Lunella_BOLD_NCBI_sl_reformatted.fasta ../${database}/${database}_db.fasta
